a328c75192064aabb03e08b3730f513c0feaf349
francisco <pacolopezbaena@gmail.com>
2015-04-17 00:09:37 +0200

    Conexion con mongodb solo consigo mostrar con el toString

--- src/main/java/com/tad3/iclass/dao/AlumnoDAO.java
+++ src/main/java/com/tad3/iclass/dao/AlumnoDAO.java
@@ -3,13 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.tad3.iclass.dao;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.tad3.iclass.entidad.Alumno;
+import java.net.UnknownHostException;
+import java.util.List;
 
 /**
  *
  * @author francisco
  */
 public class AlumnoDAO {
-    
+
+    MongoClient mongoClient;
+
+    public MongoClient conexion() throws UnknownHostException {
+        mongoClient = new MongoClient("localhost", 27017);
+
+        return mongoClient;
+    }
+
+    public List listaAlumnos() throws UnknownHostException {
+
+        MongoClient conect = conexion();
+
+        DB database = conect.getDB("iclass");
+        DBCollection coleccion = database.getCollection("iclass");
+
+        /*
+         DBCursor cursor = coleccion.find();
+         try {
+         while (cursor.hasNext()) {
+         DBObject cur = cursor.next();
+         lista.add((Alumno) cur);
+
+         }
+         } finally {
+         cursor.close();
+         }*/
+        return coleccion.find().toArray();
+    }
+
 }
--- src/main/java/com/tad3/iclass/view/AlumnoView.java
+++ src/main/java/com/tad3/iclass/view/AlumnoView.java
@@ -1,5 +1,7 @@
 package com.tad3.iclass.view;
 
+import com.tad3.iclass.dao.AlumnoDAO;
+import com.tad3.iclass.entidad.Alumno;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
@@ -9,6 +11,11 @@ import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
+import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,11 +27,13 @@ public class AlumnoView extends CustomComponent implements View {
 
     Label text = new Label();
     Label nombre = new Label("nombre");
-
+    
     Button modificar = new Button("Modificar", new Button.ClickListener() {
         @Override
         public void buttonClick(ClickEvent event) {
+            AlumnoDAO alumno = new AlumnoDAO();
             VerticalLayout panel = new VerticalLayout();
+            Label l = new Label();
             Table table = new Table();
             table.addContainerProperty("idAlumno", int.class, null);
             table.addContainerProperty("idLugar", int.class, null);
@@ -35,8 +44,20 @@ public class AlumnoView extends CustomComponent implements View {
             table.addContainerProperty("email", String.class, null);
             table.addContainerProperty("password", String.class, null);
             table.addContainerProperty("foto", String.class, null);
+            try {
+                List<Alumno> lista = alumno.listaAlumnos();
+                Iterator it = lista.iterator();
+                while(it.hasNext()){
+                    
+                    l.setValue(it.next().toString());
+                }
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(AlumnoView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             panel.addComponent(table);
             panel.addComponent(atras);
+            panel.addComponent(l);
             setCompositionRoot(panel);
 
         }
